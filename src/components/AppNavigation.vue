aca209c26a71c75057dd5333631b12fc6e5bc686
Dorotea Klara Vinski <72578471+DKVinski@users.noreply.github.com>
2022-01-18 06:32:14 +0100

    init

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,78 @@
+<template>
+    <span>
+        <v-navigation-drawer
+            app
+            v-model="drawer"
+            class="purple lighten-2"
+            dark
+            disable-resize-watcher
+        >
+            <v-list>
+                <template v-for="(item, index) in items">
+                    <v-list-tile :key="index" :to="item.url">
+                        <v-list-tile-content>
+                            {{ item.title }}
+                        </v-list-tile-content>
+                    </v-list-tile>
+                    <v-divider :key="`divider-${index}`"></v-divider>
+                </template>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar app color="purple darken-4" dark>
+            <v-toolbar-side-icon
+                class="hidden-md-and-up"
+                @click="drawer = !drawer"
+            ></v-toolbar-side-icon>
+            <v-spacer class="hidden-md-and-up"></v-spacer>
+            <router-link to="/">
+                <v-toolbar-title data-cy="titleBtn">{{
+                    appTitle
+                }}</v-toolbar-title>
+            </router-link>
+            <v-btn
+                flat
+                class="hidden-sm-and-down nav-stem"
+                to="/stem"
+                data-cy="menuBtn"
+                >STEM predmeti</v-btn
+            >
+            <v-btn
+                flat
+                class="hidden-sm-and-down nav-social"
+                to="/social"
+                data-cy="menuBtn"
+                >Društveni predmeti</v-btn
+            >
+            <v-btn
+                flat
+                class="hidden-sm-and-down nav-development"
+                to="/development"
+                data-cy="menuBtn"
+                >Daljnji razvoj</v-btn
+            >
+        </v-toolbar>
+    </span>
+</template>
+
+<script>
+export default {
+    name: 'AppNavigation',
+    data() {
+        return {
+            appTitle: 'Virtualni laboratoriji',
+            drawer: false,
+            items: [
+                { title: 'STEM predmeti', url: '/stem' },
+                { title: 'Društveni predmeti', url: '/social' }
+            ]
+        };
+    }
+};
+</script>
+
+<style scoped>
+a {
+    color: white;
+    text-decoration: none;
+}
+</style>
---
+++ src/firebase/index.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyBeUPvHd1a1yVdv5uNAjn347xMiPTigbnw',
+    authDomain: 'sap-projekt-7a23c.firebaseapp.com',
+    projectId: 'sap-projekt-7a23c',
+    storageBucket: 'sap-projekt-7a23c.appspot.com',
+    messagingSenderId: '195877428642',
+    appId: '1:195877428642:web:033925fb6e99324575e6fb'
+};
+
+firebase.initializeApp(firebaseConfig);
